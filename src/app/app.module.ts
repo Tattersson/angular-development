8b1dffb7a336e1c4368781bc3d2276e50f5c44ee
Tatu Paski <tatupaski@outlook.com>
2022-09-19 20:38:58 +0300

    Made old Angular Dev enviroment fully functional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,30 +9,30 @@ import { FeedbackComponent } from './feedback/feedback.component';
 import { FormControlComponent } from './form-control/form-control.component';
 
 //Here comes the premade materials
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {Routes, RouterModule} from "@angular/router";
-import {MatMenuModule} from '@angular/material/menu';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { Routes, RouterModule } from "@angular/router";
+import { MatMenuModule } from '@angular/material/menu';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 import { AngularTestComponent } from './angular-test/angular-test.component';
-import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
 
 
 
 
 const appRoutes: Routes = [
-  { path: 'form-control', component: FormControlComponent },
-  { path: 'feedback', component: FeedbackComponent}
+	{ path: 'form-control', component: FormControlComponent },
+	{ path: 'feedback', component: FeedbackComponent }
 ];
 
 
@@ -41,16 +41,16 @@ const appRoutes: Routes = [
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    FeedbackComponent,
-    FormControlComponent,
-    ReactiveFormComponent,
-    AngularTestComponent
+	declarations: [
+		AppComponent,
+		NavbarComponent,
+		FeedbackComponent,
+		FormControlComponent,
+		ReactiveFormComponent,
+		AngularTestComponent
 
 
-  ],
+	],
 	imports: [
 		BrowserModule,
 		FormsModule,
@@ -71,12 +71,12 @@ const appRoutes: Routes = [
 		ReactiveFormsModule,
 		RouterModule.forRoot(
 			appRoutes,
-			{enableTracing: true} // <-- debugging
+			{ enableTracing: true } // <-- debugging
 		),
 		MatButtonToggleModule
 	],
-  providers: [],
-  bootstrap: [AppComponent]
+	providers: [],
+	bootstrap: [AppComponent]
 })
 
 export class AppModule { }
