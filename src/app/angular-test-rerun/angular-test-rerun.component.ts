87cc4246ba589111defee21aa414d704abccac93
Tatu-Topias Paski <tatu@paski.fi>
2023-02-23 19:27:53 +0200

    Validators workign except NationalID

--- src/app/angular-test-rerun/angular-test-rerun.component.ts
+++ src/app/angular-test-rerun/angular-test-rerun.component.ts
@@ -15,15 +15,27 @@ export class AngularTestRerunComponent {
   loanAmount = 0;
   loanTime = 0;
 
+  //Transfer these to GET functions
 
-  firstname: string = "";
-  lastname: string = "";
-  nationalid: string = "";
-  email: string = "";
-  phone: string = "";
+  get firstname() {
+    return this.loanForm.get('firstname');
+  }
 
-  
-loanForm!: FormGroup;
+  get lastname() {
+    return this.loanForm.get('lastname');
+  }
+  get nationalid() {
+    return this.loanForm.get('nationalid');
+  }
+
+  get email() {
+    return this.loanForm.get('email');
+  }
+
+  get phone() {
+    return this.loanForm.get('phone');
+  }
+  loanForm!: FormGroup;
 
   constructor() {
     this.initializeForm();
@@ -33,24 +45,24 @@ loanForm!: FormGroup;
 
     this.loanForm = new FormGroup({
 
-      firstname: new FormControl(this.firstname, [
+      firstname: new FormControl('', [
         Validators.required,
         Validators.minLength(2),
-        Validators.pattern("[a-zA-Z ]*")
+        Validators.pattern("[A-Za-z]")
       ]),
-      lastname: new FormControl(this.lastname, [
+      lastname: new FormControl('', [
         Validators.required,
         Validators.minLength(2),
-        Validators.pattern("[a-zA-Z ]*")
+        Validators.pattern("[A-Za-z]")
       ]),
-      nationalid: new FormControl(this.nationalid, [
+      nationalid: new FormControl('', [
         Validators.required
       ]),
-      email: new FormControl(this.email, [
+      email: new FormControl('', [
         Validators.required,
         Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
       ]),
-      phone: new FormControl(this.phone, [
+      phone: new FormControl('', [
         Validators.required,
         Validators.minLength(12),
         Validators.maxLength(12),
@@ -67,31 +79,31 @@ loanForm!: FormGroup;
 
 
 
-onSubmit() {
+  onSubmit() {
 
-  var loanTime = this.loanTime;
-  var loanAmount = this.loanAmount;
+    var loanTime = this.loanTime;
+    var loanAmount = this.loanAmount;
 
-  this.loanForm = this.loanForm.value;
+    this.loanForm = this.loanForm.value;
 
 
-  console.log(loanTime);
-  console.log(loanAmount);
+    console.log(loanTime);
+    console.log(loanAmount);
 
-  console.log(this.loanForm);
-}
+    console.log(this.loanForm);
+  }
 
-calculateMonthlypay() {
-  var result = 0;
-  var roundedResult = 0;
-  result = (Math.pow((1 + 9.5 / 1200), 12 * this.loanTime) * 9.5 / 1200) / ((Math.pow((1 + 9.5 / 1200), 12 * this.loanTime)) - 1) * this.loanAmount;
+  calculateMonthlypay() {
+    var result = 0;
+    var roundedResult = 0;
+    result = (Math.pow((1 + 9.5 / 1200), 12 * this.loanTime) * 9.5 / 1200) / ((Math.pow((1 + 9.5 / 1200), 12 * this.loanTime)) - 1) * this.loanAmount;
 
-  roundedResult = parseFloat(result.toFixed(2));
+    roundedResult = parseFloat(result.toFixed(2));
 
-  console.log(roundedResult);
-  return roundedResult;
+    console.log(roundedResult);
+    return roundedResult;
 
-}
+  }
 
 
 
